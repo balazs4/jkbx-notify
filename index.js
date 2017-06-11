2aee7e95ddda1ea8b498936963a8ef6b1d38a879
balazs4 <balazs4web@gmail.com>
2017-06-11 12:06:08 +0200

    barely good enough for STAR FM From Hell :)

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
+const log = require('debug')('jukebox-songinfo');
 const request = require('request-promise-native');
 
 module.exports = ({ publish }) => async ({ payload, topic }) => {
-  const title = payload['media-title'];
+  const { metadata, filename } = payload;
 
-  const item = await request(`${process.env.SONGINFO}/${title}`, {
+  if (filename === null || filename === undefined) {
+    return; // probably the jukebox is about to stop, so just ignore it
+  }
+
+  if (metadata === null || metadata === undefined) {
+    return;
+  }
+
+  const term = metadata['icy-title'];
+
+  if (term === null || term === undefined || term === '') {
+    return;
+  }
+
+  log(`Requesting information for '${term}'...`);
+
+  const item = await request(`${process.env.SONGINFO}/${term}`, {
     method: 'GET',
     json: true,
     resolveWithFullResponse: true,
     simple: false
   });
 
-  const { body: { artist, album, title, year, cover64 } } = item;
+  const prefix = term == payload['media-title'] ? 'NEXT' : 'NOW';
+  const source = metadata['icy-name'] === 'STAR FM From Hell'
+    ? `${prefix} on ${metadata['icy-name']}`
+    : metadata['icy-name'];
 
-  const msg = {
-    icon: cover64,
-    title,
-    body: [`by ${artist}`, `from ${album}`, year].join('\n')
+  let msg = {
+    title: term,
+    body: source
   };
-  await publish('/jukebox/notification', msg);
+
+  if (item.statusCode === 200) {
+    const { body: { artist, album, title, release_date, cover } } = item;
+
+    msg = {
+      iconUrl: cover,
+      title,
+      body: [`by ${artist}`, `from ${album}`, release_date, source].join('\n')
+    };
+  }
+
+  await publish('/jukebox/songinfo', msg);
 };
